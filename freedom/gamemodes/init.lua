6ebd6565a2c3a2e27241693af06bf9d5a6c0ca43
NH-Team <34549389+NH-Team@users.noreply.github.com>
2017-12-14 13:02:02 -0800

    Create init.lua

---
+++ freedom/gamemodes/init.lua
@@ -0,0 +1,16 @@
+AddCSLuaFile( "cl_init.lua" )
+AddCSLuaFile( "shared.lua" )
+
+include( "shared.lua" )
+
+function GM:PlayerLoadout( pl )
+ 
+    pl:Give( "weapon_pistol" )
+    pl:Give( "weapon_smg1" )
+    pl:Give( "weapon_crowbar" )
+	pl:Give( "weapon_physgun" )
+	
+    pl:GiveAmmo( 999, "pistol" )
+    pl:GiveAmmo( 999, "smg1" )
+ 
+end
